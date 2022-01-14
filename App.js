abcc40a5c4114ab5d94655f413e741f1aa5a6239
BryanVizuete17 <bryans_v_17@hotail.com>
2022-01-13 21:50:04 -0500

    bvi-version inicial con firebase js

---
+++ App.js
@@ -0,0 +1,186 @@
+import React, {useEffect} from 'react';
+import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
+import { BlurView } from 'expo-blur';
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './firebase-config';
+import { getFirestore, collection,getDocs, doc, setDoc} from 'firebase/firestore';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
+
+
+const uri = 'https://static.educalingo.com/img/en/800/basketball.jpg'
+const profilePicture = 'https://scontent.fuio1-2.fna.fbcdn.net/v/t1.6435-9/161290942_244621407360440_7430627466680561360_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=RyL7fz4X3dQAX_Mvnc_&_nc_ht=scontent.fuio1-2.fna&oh=00_AT-9sXRezc6zCTAmeIrw-XZd8iqn132qieZa2KUcYOf_-w&oe=61E30E4E'
+
+
+function HomeScreen() {
+ 
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  
+  const GetData= async ()=>{
+    const citiesCol = collection(db, 'cities');
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map(doc =>doc.data());
+
+    console.log(cityList);
+
+  }
+  const SetData = async ()=>{
+    const city= 'Quito'
+    await setDoc(doc(db, "cities","1"),{
+      city_name: city,
+    });
+  }
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title='Get Data' onPress={GetData}></Button>
+      <Button title='Set Data' onPress={SetData}></Button>
+    </View>
+  );
+}
+
+  function LoginScreen() {
+
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+    const navigation = useNavigation();
+
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+    const handleCreateAccount = () => {
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Account created!')
+        const user = userCredential.user;
+        console.log(user)
+      })
+      .catch(error => {
+        console.log(error)
+        Alert.alert(error.message)
+      })
+    }
+
+    const handleSignIn = () => {
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log('Signed in!')
+        const user = userCredential.user;
+        console.log(user)
+        navigation.navigate('Home');
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+
+
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
+       <ScrollView contentContainerStyle= {{
+          flex: 1,
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}> 
+          <BlurView intensity={100}>
+            <View style={styles.login}>
+              <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
+              <View>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>E-mail</Text>
+                <TextInput onChangeText={(text) => setEmail(text)} style={styles.input} placeholder="betomoedano@outlook.com" />
+              </View>
+              <View>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Password</Text>
+                <TextInput onChangeText={(text) => setPassword(text)} style={styles.input} placeholder="password" secureTextEntry={true}/>
+              </View>
+              <TouchableOpacity onPress={handleSignIn} style={[styles.button, {backgroundColor: '#00CFEB90'}]}>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Login</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={handleCreateAccount} style={[styles.button, {backgroundColor: '#6792F090'}]}>
+                <Text style={{fontSize: 17, fontWeight: '400', color: 'white'}}>Create Account</Text>
+              </TouchableOpacity>
+            </View>
+          </BlurView>
+        </ScrollView>
+      </View>
+    );
+  }
+
+  const Stack = createNativeStackNavigator();
+  
+  export default function App() {
+  return (
+    <NavigationContainer>
+     
+      <Stack.Navigator initialRouteName="Login">
+    
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  login: {
+    width: 350,
+    height: 500,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: '#fff',
+    borderWidth: 1,
+    marginVertical: 30
+  },
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ffffff90',
+    marginBottom: 20
+  },
+  button: {
+    width: 250,
+    height: 40,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    borderColor: '#fff',
+    borderWidth: 1,
+  }
+
+});
---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDr-WC-F98Iqa0-kf48sTHKHdUdW65NcZA",
+  authDomain: "monsterapp-4afa5.firebaseapp.com",
+  projectId: "monsterapp-4afa5",
+  storageBucket: "monsterapp-4afa5.appspot.com",
+  messagingSenderId: "1051898555450",
+  appId: "1:1051898555450:web:31ce35e41df0b0c867d903"
+};
+
